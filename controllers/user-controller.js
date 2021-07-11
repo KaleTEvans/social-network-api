3d078b333f18dd9150241be207dc4e476e90fe63
KaleTEvans <kaletevans@gmail.com>
2021-07-11 18:29:56 -0500

    added ability to add friends

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -42,7 +42,63 @@ const userController = {
         User.create(body)
         .then(dbUserData => res.json(dbUserData))
         .catch(err => res.status(400).json(err))
+    },
+    // update user
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+    // delete user 
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+    // add a friend
+    addFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $addToSet: { friends: params.friendId } },
+            { new: true }
+        )
+        .then(dbFriendData => {
+            if (!dbFriendData) {
+                res.status(500).json({ message: 'no user found with that id' })
+                return;
+            }
+            res.json(dbFriendData);
+        })
+        .catch(err => res.status(400).json(err));
+    },
+    // delete a frined
+    deleteFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $pull: { friends: params.friendId } },
+            { new: true }
+        )
+        .then(dbFriendData => {
+            if (!dbFriendData) {
+                res.status(500).json({ message: 'no user found with that id' })
+                return;
+            }
+            res.json(dbFriendData);
+        })
+        .catch(err => res.status(400).json(err));
     }
-}
+};
 
 module.exports = userController;
